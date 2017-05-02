845d8d871dde03ac6aff3939a117d4c902aa51f4
CeceSoudaly <ccsoudal@yahoo.com>
2017-05-02 16:50:01 -0500

    student location not showing up.

--- MyMap/MapViewController.swift
+++ MyMap/MapViewController.swift
@@ -22,33 +22,35 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         let barButtonRefresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: "refreshButtonTouchUp")
         
         self.navigationItem.rightBarButtonItems = [barButtonRefresh, barButtonPin]
+        getLocationsOnMap()   // Get locations from Parse and set them on map annotations
+        mapView.delegate = self
+       
+    }
+    
+    func showAlert(caller: UIViewController, error: NSError) {
+        print((error.domain),(error.localizedDescription))
+        let alert = UIAlertController(title: error.domain, message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil));
+        caller.present(alert, animated: true, completion: nil)
+    }
+    
+    func getLocationsOnMap () {
         
-        
-        // The "locations" array is an array of dictionary objects that are similar to the JSON
-        // data that you can download from parse.
-        let locations = hardCodedLocationData()
-        
-        // We will create an MKPointAnnotation for each dictionary in "locations". The
-        // point annotations will be stored in this array, and then provided to the map view.
         var annotations = [MKPointAnnotation]()
         
-        // The "locations" array is loaded with the sample data below. We are using the dictionaries
-        // to create map annotations. This would be more stylish if the dictionaries were being
-        // used to create custom structs. Perhaps StudentLocation structs.
-        
-        for dictionary in locations {
+        for location in StudentLocation.sharedInstance.studentArray {
             
             // Notice that the float values are being used to create CLLocationDegree values.
             // This is a version of the Double type.
-            let lat = CLLocationDegrees(dictionary["latitude"] as! Double)
-            let long = CLLocationDegrees(dictionary["longitude"] as! Double)
+            let lat = CLLocationDegrees(location.latitude! as Float)
+            let long = CLLocationDegrees(location.longitude! as Float)
             
             // The lat and long are used to create a CLLocationCoordinates2D instance.
             let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
             
-            let first = dictionary["firstName"] as! String
-            let last = dictionary["lastName"] as! String
-            let mediaURL = dictionary["mediaURL"] as! String
+            let first = location.firstName! as String
+            let last = location.lastName! as String
+            let mediaURL = location.mediaURL! as String
             
             // Here we create the annotation and set its coordiate, title, and subtitle properties
             let annotation = MKPointAnnotation()
@@ -58,11 +60,11 @@ class MapViewController: UIViewController, MKMapViewDelegate {
             
             // Finally we place the annotation in an array of annotations.
             annotations.append(annotation)
+            
         }
         
         // When the array is complete, we add the annotations to the map.
-        self.mapView.addAnnotations(annotations)
-        
+        mapView.addAnnotations(annotations)
     }
     
     // MARK: - MKMapViewDelegate
@@ -94,76 +96,96 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     // to the URL specified in the annotationViews subtitle property.
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         if control == view.rightCalloutAccessoryView {
+            
             let app = UIApplication.shared
             if let toOpen = view.annotation?.subtitle! {
                 app.openURL(URL(string: toOpen)!)
             }
         }
     }
+    
     //    func mapView(mapView: MKMapView, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
     //
-    //        if control == annotationView.rightCalloutAccessoryView {
-    //            let app = UIApplication.sharedApplication()
-    //            app.openURL(NSURL(string: annotationView.annotation.subtitle))
+    //            if control == annotationView.rightCalloutAccessoryView {
+    //                let app = UIApplication.sharedApplication
+    //                app.openURL(NSURL(string: annotationView.annotation.subtitle))
+    //            }
     //        }
+    
+    func getStudentLocationData(_ completionHandlerForMap: @escaping (_ result: [StudentLocation]?, _ error: NSError?) -> Void) -> URLSessionTask {
+        
+        let request = NSMutableURLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!)
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        let session = URLSession.shared
+        
+        var parsedResult: AnyObject!
+        
+        
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+            if error != nil { // Handle error...
+                return
+            }
+            
+            let parsedData = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments) as! NSDictionary
+            
+            //BUILD THE ARRAY
+            parsedResult = parsedData.value(forKey: "results") as! [[String: AnyObject]] as AnyObject!
+            
+            func sendError(_ error: String) {
+                print(error)
+                let userInfo = [NSLocalizedDescriptionKey : error]
+                completionHandlerForMap(nil, NSError(domain: "taskForGETMethod", code: 1, userInfo: userInfo))
+            }
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                sendError("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                sendError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            //            guard let data = data else {
+            //                sendError("No data was returned by the request!")
+            //                return
+            //            }
+            
+            /* 5/6. Parse the data and use the data (happens in completion handler) */
+            let studentsLocations = StudentLocation.arrayFromResults(results: parsedResult as! [[String : AnyObject]])
+            completionHandlerForMap(studentsLocations, nil)
+            
+            
+            
+        }
+        
+        task.resume()
+        
+        return task
+        
+        
+    }
+    
+    
+    //    /* Helper: Given raw JSON, return a usable Foundation object */
+    //    class func parseJSONWithCompletionHandler(data: NSData, completionHandler: (_ result: AnyObject?, _ error: NSError?) -> Void) {
+    //
+    //        var parsedResult: AnyObject!
+    //        do {
+    //            parsedResult = JSONSerialization.jsonObject(with: data as Data, options: .allowFragments) as AnyObject!
+    //        } catch {
+    //            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+    //            completionHandler(nil, NSError(domain: "parseJSONWithCompletionHandler", code: 1, userInfo: userInfo))
+    //        }
+    //        
+    //        completionHandler(parsedResult, nil)
     //    }
     
-    // MARK: - Sample Data
-    
-    // Some sample data. This is a dictionary that is more or less similar to the
-    // JSON data that you will download from Parse.
-    
-    func hardCodedLocationData() -> [[String : AnyObject]] {
-        return  [
-            [
-                "createdAt" : "2015-02-24T22:27:14.456Z" as AnyObject,
-                "firstName" : "Jessica" as AnyObject,
-                "lastName" : "Uelmen" as AnyObject,
-                "latitude" : 28.1461248 as AnyObject,
-                "longitude" : -82.75676799999999 as AnyObject,
-                "mapString" : "Tarpon Springs, FL" as AnyObject,
-                "mediaURL" : "www.linkedin.com/in/jessicauelmen/en" as AnyObject,
-                "objectId" : "kj18GEaWD8" as AnyObject,
-                "uniqueKey" : 872458750 as AnyObject,
-                "updatedAt" : "2015-03-09T22:07:09.593Z" as AnyObject
-            ], [
-                "createdAt" : "2015-02-24T22:35:30.639Z" as AnyObject,
-                "firstName" : "Gabrielle" as AnyObject,
-                "lastName" : "Miller-Messner" as AnyObject,
-                "latitude" : 35.1740471 as AnyObject,
-                "longitude" : -79.3922539 as AnyObject,
-                "mapString" : "Southern Pines, NC" as AnyObject,
-                "mediaURL" : "http://www.linkedin.com/pub/gabrielle-miller-messner/11/557/60/en" as AnyObject,
-                "objectId" : "8ZEuHF5uX8" as AnyObject,
-                "uniqueKey" : 2256298598 as AnyObject,
-                "updatedAt" : "2015-03-11T03:23:49.582Z" as AnyObject
-            ], [
-                "createdAt" : "2015-02-24T22:30:54.442Z" as AnyObject,
-                "firstName" : "Jason" as AnyObject,
-                "lastName" : "Schatz" as AnyObject,
-                "latitude" : 37.7617 as AnyObject,
-                "longitude" : -122.4216 as AnyObject,
-                "mapString" : "18th and Valencia, San Francisco, CA" as AnyObject,
-                "mediaURL" : "http://en.wikipedia.org/wiki/Swift_%28programming_language%29" as AnyObject,
-                "objectId" : "hiz0vOTmrL" as AnyObject,
-                "uniqueKey" : 2362758535 as AnyObject,
-                "updatedAt" : "2015-03-10T17:20:31.828Z" as AnyObject
-            ], [
-                "createdAt" : "2015-03-11T02:48:18.321Z" as AnyObject,
-                "firstName" : "Jarrod" as AnyObject,
-                "lastName" : "Parkes" as AnyObject,
-                "latitude" : 34.73037 as AnyObject,
-                "longitude" : -86.58611000000001 as AnyObject,
-                "mapString" : "Huntsville, Alabama" as AnyObject,
-                "mediaURL" : "https://linkedin.com/in/jarrodparkes" as AnyObject,
-                "objectId" : "CDHfAy8sdp" as AnyObject,
-                "uniqueKey" : 996618664 as AnyObject,
-                "updatedAt" : "2015-03-13T03:37:58.389Z" as AnyObject
-            ]
-        ]
-    }
-
-
 
 }
 
